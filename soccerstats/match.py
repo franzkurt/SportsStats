d13be398e7f9881cfdeed8b035aaa56749760c34
Nicholas <nicholcosta@hotmail.com>
2023-04-27 17:16:30 -0300

    Atualização soccerstats

--- soccerstats/match.py
+++ soccerstats/match.py
@@ -5,15 +5,16 @@ url = 'https://www.soccerstats.com/pmatch.asp?league=italy&stats=215-5-2-2023-le
 html = requests.get(url).text
 soup = bs(html, 'html.parser')
 tag_h2 = soup.find('h2', string='Corner statistics')
-stats = {}
-stat = tag_h2.text
 empty = tag_h2.next_sibling
 tabela = empty.next_sibling
 nomes = tabela.find_all('font', {'style':'font-size:13px;font-weight:bold;'})
 homename = nomes[0].text
 awayname = nomes[1].text
 #print(homename, awayname)
-
+stats = {}
+stat = tag_h2.text
+stats[homename], stats[awayname] = {} , {}
+stats[homename][stat], stats[awayname][stat] = {}, {}
 empty = tabela.next_sibling
 data_table = empty.next_sibling
 keys = data_table.find_all('font', {'color' : '#fafafa' })
@@ -33,16 +34,22 @@ for i in dados_tags:
     x = x.strip()
     dados.append(x)
 
+x = 0
 for i in range(4,len(dados),4):
-    print(dados[i], dados[i+1],dados[i+2],dados[i+3])
-    
-print(name_dados)
+    stats[homename][stat][name_dados[x]] = {}
+    stats[awayname][stat][name_dados[x]] = {}
+    stats[homename][stat][name_dados[x]]['Home'] = dados[i]
+    stats[homename][stat][name_dados[x]]['Total'] = dados[i+1]
+    stats[awayname][stat][name_dados[x]]['Total'] = dados[i+2]
+    stats[awayname][stat][name_dados[x]]['Away']=  dados[i+3]
+    x+=1
+
+print(stats)
 
 
 
 
 
-stats[homename], stats[awayname] = {} , {}
-stats[homename][stat], stats[awayname][stat] = {}, {}
+
 
 
